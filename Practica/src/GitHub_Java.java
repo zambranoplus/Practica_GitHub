4c1fc2f14add094911e8c8c320db440729b49e01
Hernán Zambrano <zambrano12345@gmail.com>
2013-06-24 12:51:36 -0430

    Inicio de Práctica
    Java con GitHub

---
+++ Practica/src/GitHub_Java.java
@@ -0,0 +1,20 @@
+// UNET Maestría en Informática
+// Actualización Tecnológica
+// Práctica de Clases con GitHub
+//
+// Adrian
+// Alfredo
+// Hernán
+// Wilson
+
+
+public class GitHub_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

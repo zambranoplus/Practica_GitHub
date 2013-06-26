e14ab3468fe148bca5868a912e26e8ba143bae1d
corpo <corpo@Delcy>
2013-06-26 17:56:54 -0400

    actualizacion prueba

---
+++ Practica/src/TipoCuenta.java
@@ -0,0 +1,13 @@
+
+
+public class TipoCuenta {
+
+	private String descripcion;
+	private long idTipocuenta;
+
+	public void addTipoCuenta() {
+		// TODO - implement TipoCuenta.addTipoCuenta
+		throw new UnsupportedOperationException();
+	}
+
+}
